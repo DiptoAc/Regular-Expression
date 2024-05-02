d137c171530602d7ded274b6a187d326341c9aa8
Sudipto Acharjee <86905373+DiptoAc@users.noreply.github.com>
2024-05-03 00:29:06 +0600

    Create convert_string_to_vector_of_numbers.cpp

---
+++ convert_string_to_vector_of_numbers.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+template <typename T>
+using oset = tree<T, null_type, less<T>,rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define ok cout<<"ok"<<endl;
+#define endl '\n'
+#define LLONG_MAX 1e16
+const ll M =1e9+7;
+const ll N =3e5+1;
+ll n,m;
+int32_t main()
+{
+    fast
+    ll i,j=1,k=1,p,q,tc=1,cs=0;//cin>>tc;
+    while(tc--)
+    {
+        string s;
+        getline(cin, s);
+        istringstream ss(s);
+        vector <ll> temp;
+        ll tmp;
+        while(ss >> tmp) temp.push_back(tmp);
+
+
+
+        for(auto it:temp){cout<<it<<endl;}
+    }
+}
